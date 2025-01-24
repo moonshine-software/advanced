b5425c231c169f09bc5ab00372ad6e9de0aa2fe2
Alexander Nikushkin <nikushkin@mail.ru>
2025-01-24 19:01:18 +0500

    chore: style for link-group

--- src/Components/LinkGroup.php
+++ src/Components/LinkGroup.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace MoonShine\Advanced\Components;
 
-final class LinkGroup
+use MoonShine\UI\Components\MoonShineComponent;
+
+final class LinkGroup extends MoonShineComponent
 {
+    protected string $view = 'moonshine-advanced::components.link-group';
 }
