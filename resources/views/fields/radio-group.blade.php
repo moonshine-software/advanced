4820088eafac8584f76ebdcb58799d0cc29888fe
Alexander Nikushkin <nikushkin@mail.ru>
2025-01-25 11:54:26 +0500

    chore: style for checkbox-list and radio-group

--- resources/views/fields/radio-group.blade.php
+++ resources/views/fields/radio-group.blade.php
@@ -3,13 +3,13 @@
     'value',
     'values' => []
 ])
-<div {{ $attributes }}>
+<div {{ $attributes->only(['style', 'class']) }}>
     @foreach($options as $option => $label)
         <div x-id="['field-{{ $formName }}']">
-            <x-moonshine::form.label>
+            <x-moonshine::form.label class="flex items-center gap-2">
                 <x-moonshine::form.input
                     type="radio"
-                    :attributes="$attributes->except(['type', 'checked', 'value'])
+                    :attributes="$attributes->except(['type', 'checked', 'value', 'style', 'class', 'id'])
                         ->merge(['checked' => $option === $value])
                         ->merge($optionAttributes === null ? [] : $optionAttributes($option))"
                     value="{{ $option }}"
